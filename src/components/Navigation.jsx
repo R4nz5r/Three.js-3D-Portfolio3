dc6a7fcf5836824638087686e1e8c87cf97216de
R4nz5r <ragibshahriar43@gmail.com>
2025-05-14 21:48:08 +0600

    navbar done .some bug fix needed

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <ul className="nav-ul">
+      <li className="nav-li">
+        <a className="nav-link" href="#home">
+          Home
+        </a>
+      </li>
+      <li className="nav-li">
+        <a className="nav-link" href="#about">
+          About
+        </a>
+      </li>
+      <li className="nav-li">
+        <a className="nav-link" href="#work">
+          Work
+        </a>
+      </li>
+      <li className="nav-li">
+        <a className="nav-link" href="#contact">
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Navigation;

15ae2f5fc8e6454d76319ab5628192bf7630fd66
R4nz5r <ragibshahriar43@gmail.com>
2025-05-16 17:22:19 +0600

    ParallaxBackground component done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroText from "../components/HeroText";
+import ParallaxBackGround from "../components/ParallaxBackGround";
 
 const Hero = () => {
   return (
     <section className="flex items-start justify-center md:items-center md:justify-start min-h-screen overflow-hidden c-space">
       <HeroText />
+      <ParallaxBackGround/>
     </section>
   );
 };
